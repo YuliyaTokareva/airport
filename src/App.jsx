8f8b66b9b240547fb1e1400213c72f8ef6b737dd
Yuliya Tokareva <yuliya@byzantium.com.ua>
2022-07-28 23:50:43 +0300

    add first data

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,70 @@
-import React, { Component } from 'react';
-import Header from './components/header/Header.jsx';
-import Calendar from './components/calendar/Calendar.jsx';
+import React, { Component } from "react";
+import Header from "./components/header/Header.jsx";
+import Calendar from "./components/calendar/Calendar.jsx";
 
-import { getWeekStartDate, generateWeekRange } from '../src/utils/dateUtils.js';
+import { getWeekStartDate, generateWeekRange } from "../src/utils/dateUtils.js";
 
-import './common.scss';
+import "./common.scss";
+import Modal from "./components/modal/Modal.jsx";
 
 class App extends Component {
-  state = {
-    weekStartDate: new Date(),
-  };
+    state = {
+        weekStartDate: new Date(),
+        modalIsOpen: false,
+    };
+    nextWeek = () => {
+        const newWeek = new Date(
+            this.state.weekStartDate.setDate(
+                this.state.weekStartDate.getDate() + 7
+            )
+        );
+        this.setState({
+            weekStartDate: newWeek,
+        });
+    };
+    prevWeek = () => {
+        const newWeek = new Date(
+            this.state.weekStartDate.setDate(
+                this.state.weekStartDate.getDate() - 7
+            )
+        );
+        this.setState({
+            weekStartDate: newWeek,
+        });
+    };
+    today = () => {
+        this.setState({
+            weekStartDate: new Date(),
+        });
+    };
+    toggleModal = () => {
+        this.setState({
+            modalIsOpen: !this.state.modalIsOpen,
+        });
+    };
 
-  render() {
-    const { weekStartDate } = this.state;
-    const weekDates = generateWeekRange(getWeekStartDate(weekStartDate));
+    render() {
+        const { weekStartDate } = this.state;
+        const weekDates = generateWeekRange(getWeekStartDate(weekStartDate));
 
-    return (
-      <>
-        <Header />
-        <Calendar weekDates={weekDates} />
-      </>
-    );
-  }
+        return (
+            <>
+                <Header
+                    nextWeek={this.nextWeek}
+                    prevWeek={this.prevWeek}
+                    today={this.today}
+                    toggleModal={this.toggleModal}
+                />
+                {this.state.modalIsOpen ? (
+                    <Modal closeModal={this.toggleModal} />
+                ) : (
+                    ""
+                )}
+
+                <Calendar weekDates={weekDates} />
+            </>
+        );
+    }
 }
 
 export default App;
